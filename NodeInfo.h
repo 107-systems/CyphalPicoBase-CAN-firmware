163708984d08e1175d6336b40ffdb637e33cf664
generationmake <bernhard@generationmake.de>
2022-10-01 22:45:27 +0200

    updated to latest 107-Arduino-Cyphal library with unique id functions

--- NodeInfo.h
+++ NodeInfo.h
@@ -13,37 +13,10 @@
  **************************************************************************************/
 
 #include <107-Arduino-Cyphal.h>
-#include "pico/stdlib.h"
-
-/**************************************************************************************
- * DEFINES
- **************************************************************************************/
-
-#define IDSIZE 8
-
-/**************************************************************************************
- * TYPEDEF
- **************************************************************************************/
-
-union UniqueId
-{
-  uint8_t byte_buf[16];
-};
 
 /**************************************************************************************
  * CONSTANTS
  **************************************************************************************/
-
-UniqueId const UNIQUE_ID = []()
-{
-  UniqueId uid;
-  pico_unique_board_id_t pico;
-  pico_get_unique_board_id(&pico);
-  for (int i = 0; i < IDSIZE; i++) {
-    uid.byte_buf[i] = pico.id[i];
-  }
-  return uid;
-} ();
 
 static const uavcan_node_GetInfo_Response_1_0 NODE_INFO = {
     /* uavcan.node.Version.1.0 protocol_version */
@@ -56,10 +29,10 @@ static const uavcan_node_GetInfo_Response_1_0 NODE_INFO = {
     NULL,
     /* saturated uint8[16] unique_id */
     {
-      UNIQUE_ID.byte_buf[ 0], UNIQUE_ID.byte_buf[ 1], UNIQUE_ID.byte_buf[ 2], UNIQUE_ID.byte_buf[ 3],
-      UNIQUE_ID.byte_buf[ 4], UNIQUE_ID.byte_buf[ 5], UNIQUE_ID.byte_buf[ 6], UNIQUE_ID.byte_buf[ 7],
-      UNIQUE_ID.byte_buf[ 8], UNIQUE_ID.byte_buf[ 9], UNIQUE_ID.byte_buf[10], UNIQUE_ID.byte_buf[11],
-      UNIQUE_ID.byte_buf[12], UNIQUE_ID.byte_buf[13], UNIQUE_ID.byte_buf[14], UNIQUE_ID.byte_buf[15]
+      OpenCyphalUniqueId[ 0], OpenCyphalUniqueId[ 1], OpenCyphalUniqueId[ 2], OpenCyphalUniqueId[ 3],
+      OpenCyphalUniqueId[ 4], OpenCyphalUniqueId[ 5], OpenCyphalUniqueId[ 6], OpenCyphalUniqueId[ 7],
+      OpenCyphalUniqueId[ 8], OpenCyphalUniqueId[ 9], OpenCyphalUniqueId[10], OpenCyphalUniqueId[11],
+      OpenCyphalUniqueId[12], OpenCyphalUniqueId[13], OpenCyphalUniqueId[14], OpenCyphalUniqueId[15]
     },
     /* saturated uint8[<=50] name */
     {
